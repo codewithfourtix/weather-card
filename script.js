4ee2e88889731242ad44d34948ed2925a3cc1270
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-07-29 14:10:06 +0500

    Main

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-const apiUrl = "/weather";
+const apikey = 'YOUR_API_KEY'
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric"
+
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+const weatherIcon = document.querySelector('.weather-icon')
 
 async function checkWeather(city){
-    const response = await fetch(apiUrl + '?q=' + city);
-    var data = await response.json();
-    console.log(data);
-
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = data.main.temp + '°C';
-    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
+    const response = await fetch(apiUrl +'&q='+city+ `&appid=${apikey}`)
+    var data = await response.json()
+    console.log(data)
+    
+    document.querySelector('.city').innerHTML = data.name
+    document.querySelector('.temp').innerHTML = data.main.temp + '°C'
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
 
     if(data.weather[0].main == 'Clouds'){
-        weatherIcon.src = 'images/clouds.png';
-    } else if(data.weather[0].main == 'Clear'){
-        weatherIcon.src = 'images/clear.png';
-    } else if(data.weather[0].main == 'Drizzle'){
-        weatherIcon.src = 'images/drizzle.png';
-    } else if(data.weather[0].main == 'Mist'){
-        weatherIcon.src = 'images/mist.png';
+        weatherIcon.src = 'images/clouds.png'
+    }else if(data.weather[0].main == 'Clear'){
+        weatherIcon.src = 'images/clear.png'
+    }else if(data.weather[0].main == 'Drizzle'){
+        weatherIcon.src = 'images/drizzle.png'
+    }else if(data.weather[0].main == 'Mist'){
+        weatherIcon.src = 'images/mist.png'
     }
 
-    document.querySelector('.weather').style.display = 'block';
+    document.querySelector('.weather').style.display = 'block'
 }
 
-searchBtn.addEventListener('click', () => {
+searchBtn.addEventListener('click',()=>{
     checkWeather(searchBox.value);
-});
-
+})
