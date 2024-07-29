d7fcef6c8c6561ecc2d94ddec7080d2354009548
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-07-29 13:49:19 +0500

    init commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const apikey = "b71ae39ba5f8388c200f24be810bca14"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric"
+
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+const weatherIcon = document.querySelector('.weather-icon')
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl +'&q='+city+ `&appid=${apikey}`)
+    var data = await response.json()
+    console.log(data)
+    
+    document.querySelector('.city').innerHTML = data.name
+    document.querySelector('.temp').innerHTML = data.main.temp + '°C'
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
+
+    if(data.weather[0].main == 'Clouds'){
+        weatherIcon.src = 'images/clouds.png'
+    }else if(data.weather[0].main == 'Clear'){
+        weatherIcon.src = 'images/clear.png'
+    }else if(data.weather[0].main == 'Drizzle'){
+        weatherIcon.src = 'images/drizzle.png'
+    }else if(data.weather[0].main == 'Mist'){
+        weatherIcon.src = 'images/mist.png'
+    }
+
+    document.querySelector('.weather').style.display = 'block'
+}
+
+searchBtn.addEventListener('click',()=>{
+    checkWeather(searchBox.value);
+})
